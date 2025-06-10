14133ee0d4b6adcedd7e15bf6984a8916878897b
Pavel Maskal <pmaskal@syntellect.ru>
2025-06-10 14:24:17 +0500

    Добавлен WebAPI проект

---
+++ ToDo.WebAPI/Controllers/UserController/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.WebAPI.Controllers.UserController;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return new JsonResult("Good Work");
+    }
+}
